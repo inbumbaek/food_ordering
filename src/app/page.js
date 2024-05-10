0d9045452c9ea113e3b13931298b32c81665530b
inbumbaek <inbumbaek@gmail.com>
2024-05-10 16:04:34 -0700

    create login button

--- src/app/page.js
+++ src/app/page.js
@@ -8,12 +8,12 @@ export default function Home() {
         <Link className="text-primary font-semibold text-2xl" href="">
           ST PIZZA
         </Link>
-        <nav className="flex gap-4">
+        <nav className="flex items-center gap-4 text-gray-400 font-semibold">
           <Link href={''}>Home</Link>
           <Link href={''}>Menu</Link>
           <Link href={''}>About</Link>
           <Link href={''}>Contact</Link>
-          <Link href={''}>Login</Link>
+          <Link href={''} className="bg-primary rounded-full text-white px-8 py-2">Login</Link>
         </nav>
       </header>
     </>
