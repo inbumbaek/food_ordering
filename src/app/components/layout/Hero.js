cdfad24890dd0d198eb0c991d83c6090a48d4ce7
inbumbaek <inbumbaek@gmail.com>
2024-05-11 16:46:52 -0700

    add pizza image

--- src/app/components/layout/Hero.js
+++ src/app/components/layout/Hero.js
@@ -1,8 +1,16 @@
+import Image from "next/image";
+
 export default function Hero() {
   return (
-      <section>
-        <h1>Everything is better with a Pizza</h1>
-        <p>Pizza is the missing piece that makes every day complete, a simple yet delicious joy in life</p>
-      </section>
-  )
+    <section>
+      <h1>Everything is better with a Pizza</h1>
+      <p>
+        Pizza is the missing piece that makes every day complete, a simple yet
+        delicious joy in life
+      </p>
+      <div>
+        <Image src={"/pizza.png"} layout={"fill"} alt={"pizza"} />
+      </div>
+    </section>
+  );
 }
