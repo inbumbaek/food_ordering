b7ae5af955733a67b4fa83c437b81605c8b7dd3c
inbumbaek <inbumbaek@gmail.com>
2024-05-12 15:07:46 -0700

    work on button

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
-import Header from "./components/layout/Header";
-import Hero from "./components/layout/Hero";
+import Header from "@/components/layout/Header";
+import Hero from "@/components/layout/Hero";
 
 export default function Home() {
   return (
