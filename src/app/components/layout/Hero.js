45bcd984166ed755b5085237a4ce8dbd727a9d83
inbumbaek <inbumbaek@gmail.com>
2024-05-12 14:49:47 -0700

    adjust pic and text

--- src/app/components/layout/Hero.js
+++ src/app/components/layout/Hero.js
@@ -2,14 +2,20 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-    <section>
-      <h1>Everything is better with a Pizza</h1>
-      <p>
+    <section className="grid grid-cols-2">
+      <div>
+      <h1 className="text-4xl text-semibold">Everything is better with a Pizza</h1>
+      <p className="my-4 text-gray-500">
         Pizza is the missing piece that makes every day complete, a simple yet
         delicious joy in life
       </p>
       <div>
-        <Image src={"/pizza.png"} layout={"fill"} alt={"pizza"} />
+        <button>Order now</button>
+        <button>Learn more</button>
+      </div>
+      </div>
+      <div className="relative">
+        <Image src={"/pizza.png"} layout={"fill"} objectFit={'contain'} alt={"pizza"} />
       </div>
     </section>
   );
