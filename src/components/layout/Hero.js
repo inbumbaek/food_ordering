693ecb4767b9c66d7892c36e145fde73e990cd46
inbumbaek <inbumbaek@gmail.com>
2024-05-13 02:03:17 -0700

    finish work on button

--- src/components/layout/Hero.js
+++ src/components/layout/Hero.js
@@ -12,12 +12,15 @@ export default function Hero() {
           Pizza is the missing piece that makes every day complete, a simple yet
           delicious joy in life
         </p>
-        <div className="flex gap-4">
-          <button className="bg-primary flex gap-2 text-white px-8 py-2 rounded-full">
+        <div className="flex gap-4 text-sm">
+          <button className="bg-primary uppercase flex items-center gap-2 text-white px-4 py-2 rounded-full">
             Order now
             <Right />
           </button>
-          <button>Learn more</button>
+          <button className="flex gap-2 py-2 text-gray-600 font-semibold">
+            Learn more
+            <Right />
+          </button>
         </div>
       </div>
       <div className="relative">
