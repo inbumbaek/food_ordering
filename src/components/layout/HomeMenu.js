d986d67f6493dfcb50882551b44efecf79333bb2
inbumbaek <inbumbaek@gmail.com>
2024-05-15 14:41:18 -0700

    add salad 1

--- src/components/layout/HomeMenu.js
+++ src/components/layout/HomeMenu.js
@@ -1,8 +1,22 @@
+import Image from "next/image";
+
 export default function HomeMenu() {
   return (
     <section className="">
+      <div>
+        <div className="h-48 relative">
+          <Image
+            src={"/sallad1.png"}
+            layout={"fill"}
+            objectFit={"contain"}
+            alt={"salad"}
+          />
+        </div>
+      </div>
       <div className="text-center">
-        <h3 className="uppercase text-gray-600 font-semibold leading-4">Check out</h3>
+        <h3 className="uppercase text-gray-600 font-semibold leading-4">
+          Check out
+        </h3>
         <h2 className="text-primary font-bold text-4xl italic">Menu</h2>
       </div>
     </section>
