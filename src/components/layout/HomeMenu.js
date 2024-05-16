6e8ee89c7dcf7102f373699e6dcf8ac4d2a9c008
inbumbaek <inbumbaek@gmail.com>
2024-05-16 14:10:11 -0700

    css

--- src/components/layout/HomeMenu.js
+++ src/components/layout/HomeMenu.js
@@ -4,21 +4,19 @@ export default function HomeMenu() {
   return (
     <section className="">
       <div className="absolute h-full left-0 right-0 w-full justify-start">
-        <div className="h-48 w-48 absolute -left-12 text-left">
+        <div className="absolute left-0 -top-[70px] text-left -z-10">
           <Image
             src={"/sallad1.png"}
-            layout={"fill"}
-            objectFit={"contain"}
+            width={109}
+            height={189}
             alt={"salad"}
           />
         </div>
-        <div className="h-48 absolute -top-24 right-0 -z-10">
+        <div className="absolute -top-[100px] right-0 -z-10">
           <Image
             src={"/sallad2.png"}
             width={107}
             height={195}
-            layout={"fill"}
-            objectFit={"contain"}
             alt={"salad"}
           />
         </div>
