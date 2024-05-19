729093daa4027e71acb17befd33895c0e67445d1
inbumbaek <inbumbaek@gmail.com>
2024-05-19 02:46:48 -0700

    edit css

--- src/components/layout/HomeMenu.js
+++ src/components/layout/HomeMenu.js
@@ -4,7 +4,7 @@ import MenuItem from "../menu/MenuItem";
 export default function HomeMenu() {
   return (
     <section className="">
-      <div className="absolute h-full left-0 right-0 w-full justify-start">
+      <div className="absolute left-0 right-0 w-full justify-start">
         <div className="absolute left-0 -top-[70px] text-left -z-10">
           <Image src={"/sallad1.png"} width={109} height={189} alt={"salad"} />
         </div>
@@ -12,7 +12,7 @@ export default function HomeMenu() {
           <Image src={"/sallad2.png"} width={107} height={195} alt={"salad"} />
         </div>
       </div>
-      <div className="text-center">
+      <div className="text-center mb-4">
         <h3 className="uppercase text-gray-600 font-semibold leading-4">
           Check out
         </h3>
--- src/components/menu/MenuItem.js
+++ src/components/menu/MenuItem.js
@@ -1,9 +1,9 @@
 export default function MenuItem() {
   return (
-    <div className="bg-gray-200 p-4 rounded-lg text-center">
+    <div className="bg-gray-200 p-4 rounded-lg text-center group hover:bg-red hover:shadow-2xl hover:shadow-black/50 transition-all">
       <img src="/pizza.png" alt="pizza" />
       <h4 className="font-semibold text-xl my-3">Pepperoni Pizza</h4>
-      <p className="text-gray-500 text-sm"></p>
+      <p className="text-gray-500 text-sm">pizza pizza pizza</p>
       <button className="mt-4 bg-primary text-white rounded-full px-8 py-2">
         Add to card $12
       </button>
