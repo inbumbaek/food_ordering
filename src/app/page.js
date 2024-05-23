e0f29b714b4b653a9d1524f9586e4ec8817d70ea
inbumbaek <inbumbaek@gmail.com>
2024-05-23 03:45:29 -0700

    add copyright

--- src/app/page.js
+++ src/app/page.js
@@ -23,12 +23,14 @@ export default function Home() {
           mainHeader={"Contact us"}
         />
         <div className="mt-8">
-          <a className="text-4xl" href="tel:4564654654">
+          <a className="text-4xl underline text-gray-500" href="tel:4564654654">
             +45 646 54654
           </a>
         </div>
-        
       </section>
+      <footer className="border-t p-8 text-center text-gray-500 mt-16">
+        &copy; 2023 All rights reserved
+      </footer>
     </>
   );
 }
