d98554cbb79562e5cc974170d52290b6a8d9907d
inbumbaek <inbumbaek@gmail.com>
2024-05-26 11:42:37 -0700

    input register

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,13 +1,17 @@
 export default function RegisterPage() {
   return (
     <section className="mt-8">
-      <h1 className="text-center text-primary text-4xl">
+      <h1 className="text-center text-primary text-4xl mb-4">
         Register
       </h1>
-      <form>
+      <form className="block max-w-xs mx-auto">
         <input type="email" placeholder="email" />
         <input type="password" placeholder="password" />
         <button type="submit">Register</button>
+        <div className="my-4 text-center text-gray-500">
+          or login with provider
+        </div>
+        <button>Login with google</button>
       </form>
     </section>
   )
