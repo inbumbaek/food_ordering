1e8bab2d31326f9f35fb581f64368de4542cb4b3
inbumbaek <inbumbaek@gmail.com>
2024-05-26 11:47:09 -0700

    finish css reigster

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,9 +1,7 @@
 export default function RegisterPage() {
   return (
     <section className="mt-8">
-      <h1 className="text-center text-primary text-4xl mb-4">
-        Register
-      </h1>
+      <h1 className="text-center text-primary text-4xl mb-4">Register</h1>
       <form className="block max-w-xs mx-auto">
         <input type="email" placeholder="email" />
         <input type="password" placeholder="password" />
@@ -11,8 +9,11 @@ export default function RegisterPage() {
         <div className="my-4 text-center text-gray-500">
           or login with provider
         </div>
-        <button>Login with google</button>
+        <button className="flex gap-4 justify-center">
+          <Image src={"/google.png"} alt={""} width={24} height={24} />
+          Login with google
+        </button>
       </form>
     </section>
-  )
+  );
 }
