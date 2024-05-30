59844ba14334012b33ec184580de2f2514da0019
inbumbaek <inbumbaek@gmail.com>
2024-05-30 12:55:11 -0700

    add user

---
+++ src/app/models/User.js
@@ -0,0 +1,17 @@
+const { Schema } = require("mongoose");
+
+const UserSchema = new Schema({
+  email: { type: String, required: true, unique: true },
+  password: { 
+    type: String, 
+    required: true, 
+    validate: pass => {
+      if (!pass?.length || pass.length < 5) {
+        new Error('password must be at least 5 characters');
+      }
+      pass;
+    },
+  },
+});
+
+
