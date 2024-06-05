9f3aab477713e6141e1e17de2d8ad9457fcb382c
inbumbaek <inbumbaek@gmail.com>
2024-06-05 13:42:35 -0700

    test disable user

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,29 +1,45 @@
-"use client"
+"use client";
 import Image from "next/image";
-import {useState} from "react";
+import { useState } from "react";
 
 export default function RegisterPage() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [creatingUser, setCreatingUser] = useState(false);
   const [useCreated, setUserCreated] = useState(false);
   async function handleFormSubmit(ev) {
     ev.preventDefault();
     setCreatingUser(true);
-    fetch('/api/register', {
-      method: 'POST',
-      body: JSON.stringify({email, password}),
-      headers: {'Content-Type': 'application/json'},
+    fetch("/api/register", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      headers: { "Content-Type": "application/json" },
     });
     setCreatingUser(false);
   }
   return (
     <section className="mt-8">
-      <h1 className="text-center text-primary text-4xl mb-4">Register</h1>
+      <h1 className="text-center text-primary text-4xl mb-4">
+        Register
+      </h1>
       <form className="block max-w-xs mx-auto" onSubmit={handleFormSubmit}>
-        <input type="email" placeholder="email" value={email} onChange={ev => setEmail(ev.target.value)} />
-        <input type="password" placeholder="password" value={password} onChange={ev => setPassword(ev.target.value)} />
-        <button type="submit">Register</button>
+        <input
+          type="email"
+          placeholder="email"
+          value={email}
+          disabled={creatingUser}
+          onChange={(ev) => setEmail(ev.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          disabled={creatingUser}
+          onChange={(ev) => setPassword(ev.target.value)}
+        />
+        <button type="submit" disabled={creatingUser}>
+          Register
+        </button>
         <div className="my-4 text-center text-gray-500">
           or login with provider
         </div>
