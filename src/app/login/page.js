25d6e054d6b5ff4efb83f590664109eaab7402dc
inbumbaek <inbumbaek@gmail.com>
2024-06-14 23:30:01 -0700

    login page

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,9 +1,23 @@
 export default function LoginPage() {
   return (
     <section>
-      <h1 className="text-center text-primary text-4xl mb-4">
-        Login
-      </h1>
+      <h1 className="text-center text-primary text-4xl mb-4">Login</h1>
+      <form>
+        <input
+          type="email"
+          placeholder="email"
+          value={email}
+          disabled={creatingUser}
+          onChange={(ev) => setEmail(ev.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          disabled={creatingUser}
+          onChange={(ev) => setPassword(ev.target.value)}
+        />
+      </form>
     </section>
   );
 }
