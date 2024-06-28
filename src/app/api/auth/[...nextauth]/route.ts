85e4efb1402595274b0b9381353edd1580517554
inbumbaek <inbumbaek@gmail.com>
2024-06-27 23:48:11 -0700

    import user

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,5 @@
+import * as mongoose from "mongoose";
+import User from '@/models/User'
 import NextAuth from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials";
 
@@ -14,6 +16,12 @@ const handler = NextAuth({
       async authorize(credentials, req) {
         const {email, password} = credentials;
         
+        mongoose.connect(process.env.MONGO_URL);
+        const user = User.findOne({email});
+        if (user && bcrypt.compareSync(password, user.password)) {
+
+        }
+
         return null
       }
     })
