7f417c6b01916926202e5a6cee094855d9ff5b3b
inbumbaek <inbumbaek@gmail.com>
2024-06-30 11:04:36 -0700

    create logout

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -4,6 +4,7 @@ import Link from "next/link";
 
 export default function Header() {
   const session = useSession();
+  const status = session.status;
   return (
     <header className="flex items-center justify-between">
       <nav className="flex items-center gap-8 text-gray-500 font-semibold">
@@ -16,6 +17,14 @@ export default function Header() {
         <Link href={''}>Contact</Link>
       </nav>
       <nav className="flex items-center gap-4 text-gray-500 font-semibold">
+        {status === 'authenticated' && (
+          <Link
+          href={'/register'}
+          className="bg-primary rounded-full text-white px-8 py-2"
+        >
+          Logout
+        </Link>
+        )}
         <Link href={'/login'}>Login</Link>
         <Link
           href={'/register'}
