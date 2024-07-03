a667d25c72cee77c862838bdf3b7a5858c0ffadf
inbumbaek <inbumbaek@gmail.com>
2024-07-03 13:22:49 -0700

    fix User

--- src/models/User.js
+++ src/models/User.js
@@ -1,24 +1,10 @@
-import bcrypt from 'bcrypt';
-import {model, models, Schema} from "mongoose"
+import {model, models, Schema} from "mongoose";
 
 const UserSchema = new Schema({
-  email: { type: String, required: true, unique: true },
-  password: { 
-    type: String, 
-    required: true, 
-    validate: pass => {
-      if (!pass?.length || pass.length < 5) {
-        new Error('password must be at least 5 characters');
-      }
-      return pass + 'test1234';
-    },
-  },
+  name: {type: String},
+  email: {type: String, required: true, unique: true},
+  password: {type: String},
+  image: {type: String},
 }, {timestamps: true});
 
-UserSchema.post('validate', function(user) {
-  const notHashedPassword = user.password;
-  const salt = bcrypt.genSaltSync(10);
-  user.password = bcrypt.hashSync(notHashedPassword, salt);
-})
-
-export const User = models?.User || model('User, UserSchema');
+export const User = models?.User || model('User', UserSchema);
