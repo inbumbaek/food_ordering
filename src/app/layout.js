66437c3ff2f6c92665729e63ec2d05fc4c47f632
inbumbaek <inbumbaek@gmail.com>
2024-07-04 13:52:11 -0700

    error

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import { Roboto } from "next/font/google";
 import "./globals.css";
 import Header from "../components/layout/Header";
-import {AppProvider} from "@/components/AppContext";
+import {AppProvider} from "../components/AppContext";
 
 const roboto = Roboto({ subsets: ["latin"], weight: ["400", "500", "700"] });
 
@@ -12,7 +12,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className="scroll-smooth">
       <body className={roboto.className}>
         <main className="max-w-4xl mx-auto p-4">
           <AppProvider>
@@ -25,5 +25,5 @@ export default function RootLayout({ children }) {
         </main>
       </body>
     </html>
-  );
+  )
 }
