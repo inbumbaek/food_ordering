4a2985fa6629614c2176a8a92a711084e1aa5138
inbumbaek <inbumbaek@gmail.com>
2024-07-12 23:52:22 -0700

    add question mark

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -4,8 +4,8 @@ import Link from "next/link";
 
 export default function Header() {
   const session = useSession();
-  const userData = session.data.user;
-  const userName = userData.name || userData.email;
+  const userData = session.data?.user;
+  const userName = userData?.name || userData?.email;
   return (
     <header className="flex items-center justify-between">
       <nav className="flex items-center gap-8 text-gray-500 font-semibold">
