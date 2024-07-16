687be34cb5d54bc1aa82d9a04f4a0832d4470600
inbumbaek <inbumbaek@gmail.com>
2024-07-16 14:55:05 -0700

    add ProfilePage

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,8 +1,19 @@
 'use client'
 import { useSession } from "next-auth/react"
+import {redirect} from "next/navigation";
 
 export default function ProfilePage() {
   const session = useSession();
+  const {status} = session;
+
+  if (status === 'loading') {
+    return 'Loading...';
+  }
+
+  if (status === 'unauthenticated') {
+    redirect('/login');
+  }
+
   return (
     <section className="mt-8">
       <h1 className="text-center text-primary text-4xl mb-4">
