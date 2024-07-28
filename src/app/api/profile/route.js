d4d8a71fc0eae39d5147108e43c66711cbda9a4a
inbumbaek <inbumbaek@gmail.com>
2024-07-27 23:19:23 -0700

    import User

--- src/app/api/profile/route.js
+++ src/app/api/profile/route.js
@@ -1,4 +1,5 @@
 import {authOptions} from "@/app/api/auth/[...nextauth]/route";
+import {User} from "@/models/User";
 import mongoose from "mongoose";
 import { getServerSession } from "next-auth";
 
@@ -6,7 +7,13 @@ export async function PUT(req) {
   mongoose.connect(process.env.MONGO_URL);
   const data = await req.json();
   const session = await getServerSession(authOptions);
+  const email = session.user.email;
+
+  const user = await User.findOne({email});
+
   if ('name' in data) {
 
   }
+
+  return Response.json(true);
 }
