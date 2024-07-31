ea0ac5b6c8da61e258d439e36cf0f6d0d4e91b6d
inbumbaek <inbumbaek@gmail.com>
2024-07-30 23:07:50 -0700

    add notashedpassword

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -1,5 +1,6 @@
+import {User} from "@/models/User";
+import bcrypt from "bcrypt";
 import mongoose from "mongoose";
-import {User} from "../../../models/User";
 
 export async function POST(req) {
   const body = await req.json();
@@ -8,8 +9,11 @@ export async function POST(req) {
   if (!pass?.length || pass.length < 5) {
     new Error('password must be at least 5 characters');
   }
-  
-  const createdUser =  await User.create(body)
 
+  const notHashedPassword = pass;
+  const salt = bcrypt.genSaltSync(10);
+  body.password = bcrypt.hashSync(notHashedPassword, salt);
+
+  const createdUser = await User.create(body);
   return Response.json(createdUser);
 }
