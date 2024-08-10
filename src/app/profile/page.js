05561b67316c54bbf79541e053c26491c31bfff6
inbumbaek <inbumbaek@gmail.com>
2024-08-09 22:39:44 -0700

    add upload

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -33,10 +33,10 @@ export default function ProfilePage() {
   }
 
   async function handleFileChange(ev) {
-    const files = ev.files;
-    if (files?.length > 0) {
+    const files = ev.target.files;
+    if (files?.length === 1) {
       const data = new FormData;
-      data.set("files", files);
+      data.set("file", files[0]);
       await fetch("/api/upload", {
         method: "POST",
         body: null,
