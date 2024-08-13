e963b04de4e5c6ed8f2dede85824dcd3799cef30
inbumbaek <inbumbaek@gmail.com>
2024-08-13 16:16:32 -0700

    const s3Client

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -2,6 +2,14 @@ export async function POST(req) {
   const data = await req.formData()
   if (data.get('file')) {
     const file = data.get('file');
+
+    const s3Client = new S3Client({
+      region: 'us-east-1',
+      credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY,
+        secretAccessKey: process.env.AWS_SECRET_KEY,
+      }
+    })
   }
   return Response.json(true);
 }
