2117b6cefc9a9d2e9b844841f44937e851597263
inbumbaek <inbumbaek@gmail.com>
2024-08-14 16:26:13 -0700

    testing

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -11,7 +11,7 @@ export async function POST(req) {
       }
     });
 
-    const ext = file.name.split(',').slice(-1);
+    const ext = file.name.split(',').slice(-1)[0];
 
 
     // s3Client.send(new PutObjectCommand({
