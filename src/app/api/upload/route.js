4cd76d975da340e3d25bf9b97566293dd1082320
inbumbaek <inbumbaek@gmail.com>
2024-08-16 23:40:17 -0700

    add chunks

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -17,6 +17,11 @@ export async function POST(req) {
     const ext = file.name.split(',').slice(-1)[0];
     const newFileName = uniqid() + ',' + ext;
 
+    const chunks = [];
+    for await (const chunk of file.stream()) {
+      chunks.push(chunk);
+    }
+    const buffer = Buffer.concat(chunks);
 
     await s3Client.send(new PutObjectCommand({
       Bucket: 'inbum-food-ordering',
