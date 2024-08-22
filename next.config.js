a28666f5c76127e5b771d5036610e642dc47dd02
inbumbaek <inbumbaek@gmail.com>
2024-08-22 15:33:03 -0700

    add another protocol

--- next.config.js
+++ next.config.js
@@ -5,7 +5,11 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: '*.googleusercontent.com',
-      }
+      },
+      {
+        protocol: 'https',
+        hostname: 'inbum-food-ordering.s3.amazonaws.com',
+      },
     ]
   }
 };
