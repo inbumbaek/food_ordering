64dbf877e05e25fe55708dd6cca74990474a105b
inbumbaek <inbumbaek@gmail.com>
2024-09-27 15:15:13 -0700

    add path for each link

--- src/components/layout/UserTabs.js
+++ src/components/layout/UserTabs.js
@@ -1,19 +1,21 @@
-'use client'
-import Link from "next/link"
-import { usePathname } from "next/navigation"
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
-export default function UserTabs({isAdmin}) {
+export default function UserTabs({ isAdmin }) {
   const path = usePathname();
-  return(
+  return (
     <div className="flex mx-auto gap-2 tabs justify-center">
-        <Link className={'active'} href={'/profile'}>Profile</Link>
-        {isAdmin && (
-          <>
-          <Link href={'/categories'}>Categories</Link>
-          <Link href={'/menu-items'}>Menu Items</Link>
-          <Link href={'/users'}>Users</Link>
-          </>
-        )}
-      </div>
-  )
+      <Link className={path === "/profile" ? "active" : ""} href={"/profile"}>
+        Profile
+      </Link>
+      {isAdmin && (
+        <>
+          <Link href={"/categories"} className={path === "/profile" ? "active" : ""}>Categories</Link>
+          <Link href={"/menu-items"} className={path === "/profile" ? "active" : ""}>Menu Items</Link>
+          <Link href={"/users"} className={path === "/profile" ? "active" : ""}>Users</Link>
+        </>
+      )}
+    </div>
+  );
 }
