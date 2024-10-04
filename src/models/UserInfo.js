18784a1ac0dc0b7764c6c536867c592cc7a9e9c4
inbumbaek <inbumbaek@gmail.com>
2024-10-03 20:48:41 -0700

    add UserInfo.js

---
+++ src/models/UserInfo.js
@@ -0,0 +1,15 @@
+import { Schema } from "mongoose";
+
+const UserInfoSchema = new Schema(
+  {
+    email: { type: String, required: true },
+    streetAddress: { type: String },
+    postalCode: { type: String },
+    city: { type: String },
+    country: { type: String },
+    admin: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
+
+export const UserInfo = models?.UserInfo || model('UserInfo', UserInfoSchema);
