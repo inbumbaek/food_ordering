ed09422e5c342f31c61a923a829e99d3271fc6a0
inbumbaek <inbumbaek@gmail.com>
2024-10-06 15:41:49 -0700

    organize code

--- src/app/api/profile/route.js
+++ src/app/api/profile/route.js
@@ -7,14 +7,17 @@ import { UserInfo } from "../../../models/UserInfo";
 export async function PUT(req) {
   mongoose.connect(process.env.MONGO_URL);
   const data = await req.json();
+  const {name, image} = data;
+
   const session = await getServerSession(authOptions);
   const email = session.user.email;
 
-    await User.updateOne({email}, data);
+  await User.updateOne({ email }, {name, image});
+
+  // UserInfo.updateOne({email}, {})
 
   return Response.json(true);
 }
-
 
 export async function GET() {
   mongoose.connect(process.env.MONGO_URL);
@@ -23,8 +26,7 @@ export async function GET() {
   if (!email) {
     return Response.json(null);
   }
-  const user = await User.findOne({email});
-  const UserInfo = await userInfo.findOne({email});
-  return Response.json({...user, ...userInfo}
-  )
+  const user = await User.findOne({ email });
+  const UserInfo = await userInfo.findOne({ email });
+  return Response.json({ ...user, ...userInfo });
 }
