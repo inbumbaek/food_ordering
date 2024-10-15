c94f0f5d50e65ebbcfd8359721e40896ddb2b270
inbumbaek <inbumbaek@gmail.com>
2024-10-15 16:18:41 -0700

    add phone

--- src/models/UserInfo.js
+++ src/models/UserInfo.js
@@ -9,6 +9,7 @@ const UserInfoSchema = new Schema(
     postalCode: { type: String },
     city: { type: String },
     country: { type: String },
+    phone: {type: String},
     admin: { type: Boolean, default: false },
   },
   { timestamps: true }
