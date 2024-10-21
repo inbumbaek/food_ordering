75e0df5c85773f6201bb7ec3a9a825abfa2b8114
inbumbaek <inbumbaek@gmail.com>
2024-10-20 23:59:22 -0700

    try false

--- src/app/categories/page.js
+++ src/app/categories/page.js
@@ -26,7 +26,7 @@ export default function CategoriesPage() {
   return(
     <section className="mt-8 max-w-lg mx-auto">
       
-      <UserTabs isAdmin={true} />
+      <UserTabs isAdmin={false} />
       categories
     </section>
   )
