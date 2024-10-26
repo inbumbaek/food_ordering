747cf53029959486779fcd476ed02c9a6e5e8205
inbumbaek <inbumbaek@gmail.com>
2024-10-25 23:51:46 -0700

    change to data

--- src/components/UseProfile.js
+++ src/components/UseProfile.js
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react";
 
 export default function UseProfile() {
-  const [isAdmin, setIsAdmin] = useState(false);
+  const [data, setData] = useState(false);
   const [loading, setLoading] = useState(true);
   useEffect(() => {
     setLoading(true);
     fetch('/api/profile').then(response => {
       response.json().then(data => {
-        setIsAdmin(data.admin);
+        setData(data);
         setLoading(false);
       })
     })
