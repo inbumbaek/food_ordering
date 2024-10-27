58402d82b622e135d849c7ba2dc0040c062b6e28
inbumbaek <inbumbaek@gmail.com>
2024-10-26 23:55:57 -0700

    create category

--- src/app/categories/page.js
+++ src/app/categories/page.js
@@ -1,25 +1,33 @@
-'use client';
+"use client";
 import UserTabs from "../../components/layout/UserTabs";
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from "react";
 import { useProfile } from "../../components/UseProfile";
 
 export default function CategoriesPage() {
-  
-  const {loading:profileLoading, data:profileData} = useProfile();
+  const { loading: profileLoading, data: profileData } = useProfile();
 
   if (profileLoading) {
-    return 'Loading user info...'
+    return "Loading user info...";
   }
 
   if (!profileData.admin) {
-    return 'Not an admin';
+    return "Not an admin";
   }
-  
-  return(
-    <section className="mt-8 max-w-lg mx-auto">
-      
+
+  return (
+    <section className="mt-8 max-w-md mx-auto">
       <UserTabs isAdmin={true} />
-      categories
+      <form className="mt-8">
+        <div className="flex">
+          <div>
+            <label>New category name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <button type="submit">Create</button>
+          </div>
+        </div>
+      </form>
     </section>
-  )
+  );
 }
