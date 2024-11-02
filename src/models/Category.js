07efa870fcb86e050b7230709b6fa4453ed7ebd1
inbumbaek <inbumbaek@gmail.com>
2024-11-01 23:51:49 -0700

    category

--- src/models/Category.js
+++ src/models/Category.js
@@ -3,3 +3,5 @@ import {Schema} from "mongoose";
 const CategorySchema = new Schema({
   name: {type:String, required:true},
 }, {timestamps: true});
+
+export const Category = models?.Category || model('Category', CategorySchema);
