a9fd581a553321c70f52da759fcc565cac06bf37
inbumbaek <inbumbaek@gmail.com>
2024-11-13 23:39:02 -0800

    add useProfile

--- src/app/menu-items/page.js
+++ src/app/menu-items/page.js
@@ -1,6 +1,19 @@
+'use client'
 import UserTabs from "../../components/layout/UserTabs";
+import { useProfile } from "../../components/UseProfile";
 
 export default function MenuItems() {
+
+  const {lading, data} = useProfile();
+
+  if (loading) {
+    return 'Loading user info...';
+  }
+
+  if (!data.admin) {
+    return 'Not an admin.';
+  }
+
   return (
     <section className="mt-8">
       <UserTabs isAdmin={true} />
