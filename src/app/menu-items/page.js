4824a4d91b4670f82e42ae466d1f1ce303328380
inbumbaek <inbumbaek@gmail.com>
2024-11-14 23:54:07 -0800

    add css

--- src/app/menu-items/page.js
+++ src/app/menu-items/page.js
@@ -1,26 +1,27 @@
-'use client'
+"use client";
 import UserTabs from "../../components/layout/UserTabs";
 import { useProfile } from "../../components/UseProfile";
 
 export default function MenuItems() {
-
-  const {lading, data} = useProfile();
+  const { lading, data } = useProfile();
 
   if (loading) {
-    return 'Loading user info...';
+    return "Loading user info...";
   }
 
   if (!data.admin) {
-    return 'Not an admin.';
+    return "Not an admin.";
   }
 
   return (
     <section className="mt-8">
       <UserTabs isAdmin={true} />
-      <form className="mt-8">
-        <div>
-          <label>Menu item name</label>
-          <input type="text"/>
+      <form className="mt-8 max-w-md mx-auto">
+        <div className="flex items-end gap-2">
+          <div className="grow">
+            <label>Menu item name</label>
+            <input type="text" />
+          </div>
         </div>
       </form>
     </section>
