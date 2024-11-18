f459b42e9c80fdd4e7634e37d2e23fd9ed2e6064
inbumbaek <inbumbaek@gmail.com>
2024-11-18 15:38:52 -0800

    no longer need handleFileChange

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -65,32 +65,6 @@ export default function ProfilePage() {
     });
   }
 
-  async function handleFileChange(ev) {
-    const files = ev.target.files;
-    if (files?.length === 1) {
-      const data = new FormData();
-      data.set("file", files[0]);
-
-      const uploadPromise = fetch("/api/upload", {
-        method: "POST",
-        body: data,
-      }).then((response) => {
-        if (response.ok) {
-          return response.json().then((link) => {
-            setIamge(link);
-          });
-        }
-        throw new Error("Something went wrong");
-      });
-
-      await toast.promise(uploadPromise, {
-        loading: "Uploading...",
-        success: "Upload complete",
-        error: "Upload error",
-      });
-    }
-  }
-
   if (status === "loading" || !profileFetched) {
     return "Loading...";
   }
