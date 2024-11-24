bc41e25f6d737c3b4e981728557e839c705dc5f4
inbumbaek <inbumbaek@gmail.com>
2024-11-23 20:40:38 -0800

    add style

--- src/app/menu-items/page.js
+++ src/app/menu-items/page.js
@@ -20,8 +20,10 @@ export default function MenuItemsPage() {
     <section className="mt-8">
       <UserTabs isAdmin={true} />
       <form className="mt-8 max-w-md mx-auto">
-        <div className="flex items-start gap-2">
-          <div>
+        <div
+          className="grid items-start gap-4"
+          style={{gridTemplateColumns:'.3fr .7fr'}}>
+        <div>
             <EditableImage link={image} setLink={setImage} />
           </div>
           <div className="grow">
