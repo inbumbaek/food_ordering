33937e19f41deb3edfb831bb7ddb0f1b21ddf3b9
inbumbaek <inbumbaek@gmail.com>
2024-11-24 15:05:30 -0800

    add promise

--- src/app/menu-items/page.js
+++ src/app/menu-items/page.js
@@ -14,11 +14,23 @@ export default function MenuItemsPage() {
   async function handleFormSubmit(ev) {
     ev.preventDefault();
     const data = {image,name,description,basePrice,};
-    const response = await fetch('/api/menu-items', {
+    const savingPromise = new Promise(async (resolve, reject) => {
+      const response = await fetch('/api/menu-items', {
       method: 'POST',
       body: JSON.stringify(data),
       headers: { 'Content-Type': 'application/json' },
       });
+      if (response.ok)
+        resolve();
+      else
+        reject();
+    });
+
+    await toast.promise(savingPromise, {
+      loading: 'Saving this tasty item',
+      success: 'Saved',
+      error: 'Error',
+    })
   }
 
   if (loading) {
