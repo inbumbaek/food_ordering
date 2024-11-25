d2358694d8a387be7ea7e18e24517cd262c4a3a4
inbumbaek <inbumbaek@gmail.com>
2024-11-25 03:21:32 -0800

    make menuitem route

--- src/app/api/menu-items/route.js
+++ src/app/api/menu-items/route.js
@@ -1,4 +1,9 @@
+import mongoose from "mongoose";
+import { MenuItem } from "../../../models/MenuItem";
+
 export async function POST(req) {
+  mongoose.connect(process.env.MONGO_URL);
   const data = await req.json();
-  
+  const MenuItemDoc = await MenuItem.create(data);
+  return Response.json(menuItemDoc);
 }
