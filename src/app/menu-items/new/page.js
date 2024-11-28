5fbf4a65966bc70eb2df715e06a2905ec5b70373
inbumbaek <inbumbaek@gmail.com>
2024-11-27 23:58:03 -0800

    cut and paste

--- src/app/menu-items/new/page.js
+++ src/app/menu-items/new/page.js
@@ -1,18 +1,52 @@
+import EditableImage from "../../../components/layout/EditableImage";
+import UserTabs from "../../../components/layout/UserTabs";
 import { useProfile } from "../../../components/UseProfile";
 
 export default function NewMenuItemPage() {
-  
-  const {loading, data} = useProfile();
+  const { loading, data } = useProfile();
 
   if (loading) {
-    return 'Loading user info...';
+    return "Loading user info...";
   }
 
   if (!data.admin) {
-    return 'Not an admin.';
+    return "Not an admin.";
   }
 
   return (
-
+    <section className="mt-8">
+      <UserTabs isAdmin={true} />
+      <form onSubmit={handleFormSubmit} className="mt-8 max-w-md mx-auto">
+        <div
+          className="grid items-start gap-4"
+          style={{ gridTemplateColumns: ".3fr .7fr" }}
+        >
+          <div>
+            <EditableImage link={image} setLink={setImage} />
+          </div>
+          <div className="grow">
+            <label>Item name</label>
+            <input
+              type="text"
+              value={name}
+              onChange={(ev) => setName(ev.target.value)}
+            />
+            <label>Description</label>
+            <input
+              type="text"
+              value={description}
+              onChange={(ev) => setDescription(ev.target.value)}
+            />
+            <label>Base price</label>
+            <input
+              type="text"
+              value={basePrice}
+              onChange={(ev) => setBasePrice(ev.target.value)}
+            />
+            <button type="submit">Save</button>
+          </div>
+        </div>
+      </form>
+    </section>
   );
 }
