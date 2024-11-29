5b14fd18e97743b7995aeea358a95124e681b465
inbumbaek <inbumbaek@gmail.com>
2024-11-28 22:36:29 -0800

    cut and paste

--- src/app/menu-items/new/page.js
+++ src/app/menu-items/new/page.js
@@ -3,7 +3,35 @@ import UserTabs from "../../../components/layout/UserTabs";
 import { useProfile } from "../../../components/UseProfile";
 
 export default function NewMenuItemPage() {
+
+  const [image, setImage] = useState('');
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [basePrice, setBasePrice] = useState('');
   const { loading, data } = useProfile();
+
+  async function handleFormSubmit(ev) {
+    ev.preventDefault();
+    const data = {image,name,description,basePrice,};
+    const savingPromise = new Promise(async (resolve, reject) => {
+      const response = await fetch('/api/menu-items', {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: { 'Content-Type': 'application/json' },
+      });
+      if (response.ok)
+        resolve();
+      else
+        reject();
+    });
+
+    await toast.promise(savingPromise, {
+      loading: 'Saving this tasty item',
+      success: 'Saved',
+      error: 'Error',
+    })
+  }
+
 
   if (loading) {
     return "Loading user info...";
--- src/app/menu-items/page.js
+++ src/app/menu-items/page.js
@@ -5,34 +5,7 @@ import { useProfile } from "../../components/UseProfile";
 
 export default function MenuItemsPage() {
 
-  const [image, setImage] = useState('');
-  const [name, setName] = useState('');
-  const [description, setDescription] = useState('');
-  const [basePrice, setBasePrice] = useState('');
-  const { loading, data } = useProfile();
-
-  async function handleFormSubmit(ev) {
-    ev.preventDefault();
-    const data = {image,name,description,basePrice,};
-    const savingPromise = new Promise(async (resolve, reject) => {
-      const response = await fetch('/api/menu-items', {
-      method: 'POST',
-      body: JSON.stringify(data),
-      headers: { 'Content-Type': 'application/json' },
-      });
-      if (response.ok)
-        resolve();
-      else
-        reject();
-    });
-
-    await toast.promise(savingPromise, {
-      loading: 'Saving this tasty item',
-      success: 'Saved',
-      error: 'Error',
-    })
-  }
-
+  
   if (loading) {
     return "Loading user info...";
   }
