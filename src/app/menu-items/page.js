7ce6bbc6f706ad2ab3c2e3440ad8c91f84b59732
inbumbaek <inbumbaek@gmail.com>
2024-12-10 00:32:50 -0800

    adjust css

--- src/app/menu-items/page.js
+++ src/app/menu-items/page.js
@@ -4,17 +4,16 @@ import UserTabs from "../../components/layout/UserTabs";
 import { useProfile } from "../../components/UseProfile";
 
 export default function MenuItemsPage() {
-
   const [menuItems, setMenuItems] = useState([]);
   const { loading, data } = useProfile();
 
   useEffect(() => {
-    fetch('/api/menu-items').then(res => {
-      res.json().then(menuItems => {
+    fetch("/api/menu-items").then((res) => {
+      res.json().then((menuItems) => {
         setMenuItems(menuItems);
-      })
-    })
-  }, [])
+      });
+    });
+  }, []);
 
   if (loading) {
     return "Loading user info...";
@@ -28,23 +27,27 @@ export default function MenuItemsPage() {
     <section className="mt-8 max-w-md mx-auto">
       <UserTabs isAdmin={true} />
       <div className="mt-8">
-        <Link
-          className="button flex"
-          href={"/menu-items/new"}>
+        <Link className="button flex" href={"/menu-items/new"}>
           <span>Create new menu item</span>
           <Right />
         </Link>
       </div>
       <div>
         <h2 className="text-sm text-gray-500 mt-8">Edit menu item:</h2>
-        {menuItems?.length > 0 && menuItems.map(item => (
-          <Link href={'/menu-items/edit/'+item._id} className="button mb-1">
-            <div className="relative">
-              <Image src={item.image} alt={''} width={100} height={100} />
-            </div>
-            {item.name}
-          </Link>
-        ))}
+        <div className="grid grid-cols-3 gap-2">
+          {menuItems?.length > 0 &&
+            menuItems.map((item) => (
+              <Link
+                href={"/menu-items/edit/" + item._id}
+                className="button mb-1 flex-col"
+              >
+                <div className="relative">
+                  <Image src={item.image} alt={""} width={100} height={100} />
+                </div>
+                <div className="text-center">{item.name}</div>
+              </Link>
+            ))}
+        </div>
       </div>
     </section>
   );
