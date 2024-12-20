87afbf6b8dcb5031eb3c800d6f0d2124ccb7f839
inbumbaek <inbumbaek@gmail.com>
2024-12-19 23:53:54 -0800

    add data

--- src/app/menu-items/edit/[id]/page.js
+++ src/app/menu-items/edit/[id]/page.js
@@ -24,6 +24,7 @@ export default function EditMenuItemPage() {
 
   async function handleFormSubmit(ev, data) {
     ev.preventDefault();
+    data = {...data, _id:id};
     const savingPromise = new Promise(async (resolve, reject) => {
       const response = await fetch('/api/menu-items', {
       method: 'POST',
