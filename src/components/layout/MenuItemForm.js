69f17f5aaa63fae79042b46a05f458d79f89d6f0
inbumbaek <inbumbaek@gmail.com>
2024-12-20 23:36:30 -0800

    create addSize

--- src/components/layout/MenuItemForm.js
+++ src/components/layout/MenuItemForm.js
@@ -6,6 +6,15 @@ export default function MenuItemForm({onSubmit,menuItem}) {
   const [name, setName] = useState(menuItem.name || '');
   const [description, setDescription] = useState(menuItem.description || '');
   const [basePrice, setBasePrice] = useState(menuItem.basePrice || '');
+  const [sizes, setSizes] = useState([]);
+
+  function addSize() {
+    setSizes(oldSizes => {
+      return [...oldSizes, {name:'', price:0}];
+    })
+  }
+
+
   return (
     <form 
       onSubmit={ev => onSubmit(ev, {image,name,description,basePrice})} 
@@ -38,7 +47,10 @@ export default function MenuItemForm({onSubmit,menuItem}) {
           />
           <div className="bg-gray-200 p-2 rounded-md mb-2">
             <label>Sizes</label>
-            <button className="bg-white">Add item size</button>
+            <button
+              type="button"
+              onClick={addSize}
+              className="bg-white">Add item size</button>
           </div>
           <button type="submit">Save</button>
         </div>
