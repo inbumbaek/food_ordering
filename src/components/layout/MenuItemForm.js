e273ca65215d1aa44b2789dc325fed629aa81afd
inbumbaek <inbumbaek@gmail.com>
2024-12-21 23:56:27 -0800

    add sizes

--- src/components/layout/MenuItemForm.js
+++ src/components/layout/MenuItemForm.js
@@ -1,24 +1,24 @@
 import EditableImage from "./EditableImage";
 import { useState } from "react";
 
-export default function MenuItemForm({onSubmit,menuItem}) {
-  const [image, setImage] = useState(menuItem?.image || '');
-  const [name, setName] = useState(menuItem.name || '');
-  const [description, setDescription] = useState(menuItem.description || '');
-  const [basePrice, setBasePrice] = useState(menuItem.basePrice || '');
+export default function MenuItemForm({ onSubmit, menuItem }) {
+  const [image, setImage] = useState(menuItem?.image || "");
+  const [name, setName] = useState(menuItem.name || "");
+  const [description, setDescription] = useState(menuItem.description || "");
+  const [basePrice, setBasePrice] = useState(menuItem.basePrice || "");
   const [sizes, setSizes] = useState([]);
 
   function addSize() {
-    setSizes(oldSizes => {
-      return [...oldSizes, {name:'', price:0}];
-    })
+    setSizes((oldSizes) => {
+      return [...oldSizes, { name: "", price: 0 }];
+    });
   }
 
-
   return (
-    <form 
-      onSubmit={ev => onSubmit(ev, {image,name,description,basePrice})} 
-      className="mt-8 max-w-md mx-auto">
+    <form
+      onSubmit={(ev) => onSubmit(ev, { image, name, description, basePrice })}
+      className="mt-8 max-w-md mx-auto"
+    >
       <div
         className="grid items-start gap-4"
         style={{ gridTemplateColumns: ".3fr .7fr" }}
@@ -47,10 +47,24 @@ export default function MenuItemForm({onSubmit,menuItem}) {
           />
           <div className="bg-gray-200 p-2 rounded-md mb-2">
             <label>Sizes</label>
-            <button
-              type="button"
-              onClick={addSize}
-              className="bg-white">Add item size</button>
+            {sizes?.length > 0 &&
+              sizes.map(size => (
+                <div>
+                  <input
+                    type="text"
+                    placeholder="Size name"
+                    value={size.name}
+                  />
+                  <input
+                    type="number"
+                    placeholder="Extra price"
+                    value={size.price}
+                  />
+                </div>
+              ))}
+            <button type="button" onClick={addSize} className="bg-white">
+              Add item size
+            </button>
           </div>
           <button type="submit">Save</button>
         </div>
