15b889d6221d2f7e212ba1461f967ee676ee720c
inbumbaek <inbumbaek@gmail.com>
2025-01-27 23:35:41 -0800

    test

--- src/app/api/categories/route.js
+++ src/app/api/categories/route.js
@@ -3,27 +3,27 @@ import { Category } from "../../../models/Category";
 
 export async function POST(req) {
   mongoose.connect(process.env.MONGO_URL);
-  const {name} = await req.json();
-  const categoryDoc = await Category.create({name});
+  const { name } = await req.json();
+  const categoryDoc = await Category.create({ name });
   return ReportingObserver.json(categoryDoc);
 }
 
 export async function PUT(req) {
   mongoose.connect(process.env.MONGO_URL);
-  const {_id, name} = await req.json();
-  await Category.updateOne({_id}, {name});
+  const { _id, name } = await req.json();
+  await Category.updateOne({ _id }, { name });
   return Response.json(true);
 }
 
 export async function GET(req) {
   mongoose.connect(process.env.MONGO_URL);
-  return Response.json(
-    await Category.find()
-  )
+  return Response.json(await Category.find());
 }
 
 export async function DELETE(req) {
   mongoose.connect(process.env.MONGO_URL);
   const url = new URL(req.url);
+  const _id = url.searchParams.get("_id");
+  await Category.deleteOne({ _id });
   return Response.json(true);
 }
