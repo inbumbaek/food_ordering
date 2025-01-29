ac5859ae1a685adb6badbe0c745eb4d2cfff3f66
inbumbaek <inbumbaek@gmail.com>
2025-01-28 23:59:39 -0800

    test

--- src/app/api/categories/route.js
+++ src/app/api/categories/route.js
@@ -24,6 +24,6 @@ export async function DELETE(req) {
   mongoose.connect(process.env.MONGO_URL);
   const url = new URL(req.url);
   const _id = url.searchParams.get("_id");
-  await Category.deleteOne({ _id });
+  // await Category.deleteOne({ _id });
   return Response.json(true);
 }
