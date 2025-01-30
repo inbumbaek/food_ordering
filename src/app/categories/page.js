adb877ed8af1680e2bd2cdffc64dbd3e2a1b4416
inbumbaek <inbumbaek@gmail.com>
2025-01-29 23:09:46 -0800

    add cancel button

--- src/app/categories/page.js
+++ src/app/categories/page.js
@@ -99,10 +99,11 @@ export default function CategoriesPage() {
               onChange={(ev) => setCategoryName(ev.target.value)}
             />
           </div>
-          <div className="pb-2">
+          <div className="pb-2 flex gap-2">
             <button className="border border-primary" type="submit">
               {editecdCategory ? "Update" : "Create"}
             </button>
+            <button>Cancel</button>
           </div>
         </div>
       </form>
@@ -110,7 +111,7 @@ export default function CategoriesPage() {
         <h2 className="mt-8 text-sm text-gray-500">Existing categories:</h2>
         {categories?.length > 0 &&
           categories.map((c) => (
-            <div className="bg-gray-100 rounded-xl p-2 px-4 flex gap-1 mb-1">
+            <div className="bg-gray-100 rounded-xl p-2 px-4 flex gap-1 mb-1 items-center">
               <div className="grow">
                 {c.name}
               </div>
