facaf0eb0215c5d57d1634691acbc88a08a16f34
inbumbaek <inbumbaek@gmail.com>
2025-02-03 23:47:57 -0800

    adjust css

--- src/app/menu-items/edit/[id]/page.js
+++ src/app/menu-items/edit/[id]/page.js
@@ -1,53 +1,50 @@
-'use client'
+"use client";
 
 import MenuItemForm from "@/components/layout/MenuItemForm";
 import { useParams } from "next/navigation";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 
 export default function EditMenuItemPage() {
-  
-  const {id} = useParams();
+  const { id } = useParams();
 
   const [menuItem, setMenuItem] = useState(null);
   const [redirectToItems, setRedirectToItems] = useState(false);
   const { loading, data } = useProfile();
 
   useEffect(() => {
-    fetch('/api/menu-items').then(res => {
-      res.json().then(items => {
-        const item = items.find(i => i._id === id);
+    fetch("/api/menu-items").then((res) => {
+      res.json().then((items) => {
+        const item = items.find((i) => i._id === id);
         setMenuItem(item);
       });
-    })
+    });
   }, []);
 
   async function handleFormSubmit(ev, data) {
     ev.preventDefault();
-    data = {...data, _id:id};
+    data = { ...data, _id: id };
     const savingPromise = new Promise(async (resolve, reject) => {
-      const response = await fetch('/api/menu-items', {
-      method: 'POST',
-      body: JSON.stringify(data),
-      headers: { 'Content-Type': 'application/json' },
+      const response = await fetch("/api/menu-items", {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: { "Content-Type": "application/json" },
       });
-      if (response.ok)
-        resolve();
-      else
-        reject();
+      if (response.ok) resolve();
+      else reject();
     });
 
     await toast.promise(savingPromise, {
-      loading: 'Saving this tasty item',
-      success: 'Saved',
-      error: 'Error',
-    })
+      loading: "Saving this tasty item",
+      success: "Saved",
+      error: "Error",
+    });
 
     setRedirectToItems(true);
   }
 
   if (redirectToItems) {
-    return redirect('/menu-items');
+    return redirect("/menu-items");
   }
 
   if (loading) {
@@ -62,14 +59,16 @@ export default function EditMenuItemPage() {
     <section className="mt-8">
       <UserTabs isAdmin={true} />
       <div className="max-w-md mx-auto mt-8">
-        <Link href={'/menu-items'} className="button">
+        <Link href={"/menu-items"} className="button">
           <Left />
           <span>Show all menu items</span>
         </Link>
       </div>
-      <MenuItemForm menuItem={menuItem} onSubmit={handleFormSubmit}/>
+      <MenuItemForm menuItem={menuItem} onSubmit={handleFormSubmit} />
       <div className="max-w-md mx-auto mt-4">
-        <button>Delete this menu item</button>
+        <div className="max-w-xs ml-auto pl-4">
+          <button>Delete this menu item</button>
+        </div>
       </div>
     </section>
   );
