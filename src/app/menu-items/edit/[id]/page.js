312df920777736b0fb493881e33318c56cb63a35
inbumbaek <inbumbaek@gmail.com>
2025-02-12 23:57:34 -0800

    add button

--- src/app/menu-items/edit/[id]/page.js
+++ src/app/menu-items/edit/[id]/page.js
@@ -84,7 +84,7 @@ export default function EditMenuItemPage() {
         </Link>
       </div>
       <MenuItemForm menuItem={menuItem} onSubmit={handleFormSubmit} />
-      <div className="max-w-md mx-auto mt-4">
+      <div className="max-w-md mx-auto mt-2">
         <div className="max-w-xs ml-auto pl-4">
           <DeleteButton label="Delete this menu item" onDelete={handelDeleteClick}/>
         </div>
--- src/components/DeleteButton.js
+++ src/components/DeleteButton.js
@@ -1,6 +1,20 @@
 export default function DeleteButton({label,onDelete}) {
+  const [showConfirm, setShowConfirm] = useState(false);
+
+  if (showConfirm) {
+    return(
+      <div className="flex gap-2">
+        <button type="button">
+          Cancel
+        </button>
+        <button type="button">
+          Yes, delete!
+        </button>
+      </div>
+    )
+  }
   return (
-    <button type="button">
+    <button type="button" onClick={() => setShowConfirm(true)}>
       {label}
     </button>
   );
