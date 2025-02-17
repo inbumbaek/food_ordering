631a744037ee5aef368ee1e33ec0da07ec022905
inbumbaek <inbumbaek@gmail.com>
2025-02-16 17:18:23 -0800

    move to center

--- src/components/DeleteButton.js
+++ src/components/DeleteButton.js
@@ -3,15 +3,17 @@ export default function DeleteButton({ label, onDelete }) {
 
   if (showConfirm) {
     return (
-      <div className="fixed top-0 left-0 bg-white p-4 rounded-lg">
-        <div>Are you sure you want to delete?</div>
-        <div className="flex gap-2 mt-1">
-          <button type="button" onClick={() => setShowConfirm(false)}>
-            Cancel
-          </button>
-          <button onClick={onDelete} type="button" className="primary">
-            Yes, delete!
-          </button>
+      <div className="fixed bg-black/80 inset-0 flex items-center h-full justify-center">
+        <div className="fixed top-0 left-0 bg-white p-4 rounded-lg">
+          <div>Are you sure you want to delete?</div>
+          <div className="flex gap-2 mt-1">
+            <button type="button" onClick={() => setShowConfirm(false)}>
+              Cancel
+            </button>
+            <button onClick={onDelete} type="button" className="primary">
+              Yes,&nbsp;delete!
+            </button>
+          </div>
         </div>
       </div>
     );
